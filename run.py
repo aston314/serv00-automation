c79d89a9aeec208544bbe4238c6d583db333cbaa
aston314 <aston.he@gmail.com>
2024-07-18 11:03:11 +0800

    Update run.py

--- run.py
+++ run.py
@@ -35,27 +35,6 @@ time = datetime.now(beijing_timezone).strftime('%Y-%m-%d %H:%M:%S')
 
 loginip = requests.get('https://api.ipify.org?format=json').json()['ip']
 
-#pushplus_token = os.getenv('PUSHPLUS_TOKEN')
-
-# title = 'serv00 服务器登录提醒'
-# content = f"用户：{', '.join(user_list)}, 登录了 SSH 服务器<br>登录时间：{time}<br>登录IP：{loginip}"
-# url = 'http://www.pushplus.plus/send'
-# data = {
-#     "token": pushplus_token,
-#     "title": title,
-#     "content": content
-# }
-# body = json.dumps(data).encode(encoding='utf-8')
-# headers = {'Content-Type': 'application/json'}
-
-# response = requests.post(url, data=body, headers=headers)
-# if response.status_code == 200:
-#     print("推送成功")
-# else:
-#     print(f"推送失败，状态码: {response.status_code}")
-
-#import requests
-
 title = 'serv00 服务器登录提醒'
 content = f"用户：{', '.join(user_list)}, 登录了 SSH 服务器\n登录时间：{time}\n登录IP：{loginip}"
 
@@ -69,7 +48,6 @@ headers = {
 }
 
 response = requests.post(url, data=content.encode(encoding='utf-8'), headers=headers )
-#response = requests.post(url, data=body, headers=headers)
 
 if response.status_code == 200:
     print("推送成功")
